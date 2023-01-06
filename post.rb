525d99d92199d9a1eba10d0127a47f6c8ccda419
Alex Nuzhniy <alexnuzhniy@MacBook-Pro.local>
2023-01-06 13:18:36 +0300

    new classes

--- post.rb
+++ post.rb
@@ -1,32 +1,41 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+    @text = []
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
   end
 
   def read_from_console
-
   end
 
   def to_strings
 
-  def save
-    file = File.new(file_path)
+  end
 
-    for item in to_strings do
-      file.puts(item)
-    end
+  def save
+    file = File.new(file_path, 'w:UTF-8') # открываем файл на запись
+
+    to_strings.each { |string| file.puts(string) }
 
     file.close
   end
 
+
   def file_path
     current_path = File.dirname(__FILE__)
 
-    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+    file_time = @created_at.strftime('%Y-%m-%d_%H-%M-%S')
 
-    return current_path + "/" + file_name
+    "#{current_path}/#{self.class.name}_#{file_time}.txt"
   end
 
 end
